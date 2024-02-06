03a31bf69464b044c7848133b9b37012443e6aeb
darrenb <darren.buns74@outlook.com>
2024-02-06 19:51:47 +0000

    Add method for creating rule classes with reflection

--- Classes/RunFizzBuzz.cs
+++ Classes/RunFizzBuzz.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzz.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,29 @@ namespace FizzBuzz.Classes
 {
     internal class RunFizzBuzz
     {
+        private List<INumberChecker> _numberCheckers = new List<INumberChecker>();
         public void Run()
         {
-
+            GetNumberCheckers();
         }
 
+        private void GetNumberCheckers()
+        {
+            var interfaceType = typeof(INumberChecker);
+
+            //Search assemblies for classes implementing the interface
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(x => x.GetTypes())
+                        .Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass);
+
+            foreach(var type in types)
+            {
+                var instance=Activator.CreateInstance(type);
+                if(instance != null)
+                {
+                    _numberCheckers.Add((INumberChecker)instance);
+                }
+            }
+        }
     }
 }
